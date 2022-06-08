a99f1d35c9e901305c95fd30daee4182584ce635
gybgivee <kmcn0512@gmail.com>
2022-06-08 12:54:50 +0100

    complete

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,9 +6,16 @@ class TodoList {
 
   create(myTodoList) {
 
-    const item = { id: this.id, text: myTodoList, status: 'incomplete' }
+    let currentdate = this.id.toString()+"/6/22"
+        // let datetime = (currentdate.getDate() + this.id) + "/"
+        //     + (currentdate.getMonth() + 1) + "/"
+        //     + currentdate.getFullYear()
+
+    const item = { id: this.id, text: myTodoList, status: 'incomplete', date: currentdate}
     this.id++
     this.items.push(item)
+    // console.log(item)
+    // console.log(currentdate)
     return item;
   }
   limitGetAll() {
@@ -22,24 +29,24 @@ class TodoList {
 
       for (let i = 0; i < myTodoList.length; i++) {
 
-        console.log('myTodoList ' + myTodoList[i].text);
+        // console.log('myTodoList ' + myTodoList[i].text);
         collectChar = collectChar+myTodoList[i].text ;
         
        
 
       }
       const size=collectChar.length;
-      console.log('collectChar ' + collectChar);
-      console.log('size'+size);
+      // console.log('collectChar ' + collectChar);
+      // console.log('size'+size);
       mySubstring = collectChar.substring(0,limit);
      
       mySubstring=mySubstring+'...';
-      console.log('mySubstring '+mySubstring+' lenght :'+ mySubstring.length);
+      // console.log('mySubstring '+mySubstring+' lenght :'+ mySubstring.length);
 
     }else{
 
       mySubstring=myTodoList[0].text;
-      console.log('mySubstring '+mySubstring);
+      // console.log('mySubstring '+mySubstring);
     }
     return mySubstring;
 
@@ -70,16 +77,38 @@ class TodoList {
     const index = this.items.indexOf(item)
     return this.items.splice(index, 1)[0]
   }
+
+  findByDate(date) {
+    const allItems = this.items
+    const result = allItems.find(element => element.date === date);
+    // console.log('result '+result);
+
+
+    // console.log(result)
+    // console.log(allItems[1].id)
+    // console.log("item length is: ", allItems.length)
+
+    return result
+  }
+
 }
 const myTodoList = new TodoList();
+/*
+myTodoList.create("Shopping", "1/6/22");
+myTodoList.create("Laudry", "2/6/22");
+myTodoList.create("gardenning", "3/6/22");
+*/
 myTodoList.create("Shopping");
-// myTodoList.create("Laudry");
-// myTodoList.create("gardenning");
+myTodoList.create("Laudry");
+myTodoList.create("gardenning");
+myTodoList.findByDate("1/06/22");
 // myTodoList.create("go to the park");
 const lists = myTodoList.limitGetAll();
 // for (const iterator of lists) {
 //   console.log('list : '+iterator);
 // }
+console.log(myTodoList.findByDate("2/6/22"))
+
 
 
 module.exports = TodoList
